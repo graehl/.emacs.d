c2a8bc713b929666e6972aaba6b18614d39d5306
graehl <graehl@gmail.com>
2011-12-08 10:45:10 -0800

    C-x C-r ido recent

--- setup-ido.el
+++ setup-ido.el
@@ -11,7 +11,36 @@
 ;; Always rescan buffer for imenu
 (set-default 'imenu-auto-rescan t)
 
+(add-to-list 'ido-ignore-directories ".svn")
+(add-to-list 'ido-ignore-directories ".git")
 (add-to-list 'ido-ignore-directories "target")
 (add-to-list 'ido-ignore-directories "node_modules")
+
+(defun recentf-open-files-compl ()
+  (interactive)
+  (let* ((tocpl (mapcar (lambda (x) (cons (file-name-nondirectory x) x))
+                        recentf-list))
+         (fname (completing-read "File name: " tocpl nil nil)))
+    (when fname
+      (find-file (cdr (assoc-string fname tocpl))))))
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let* ((file-assoc-list
+          (mapcar (lambda (x)
+                    (cons (file-name-nondirectory x)
+                          x))
+                  recentf-list))
+         (filename-list
+          (remove-duplicates (mapcar #'car file-assoc-list)
+                             :test #'string=))
+         (filename (ido-completing-read "Choose recent file: "
+                                        filename-list
+                                        nil
+                                        t)))
+    (when filename
+      (find-file (cdr (assoc filename
+                             file-assoc-list))))))
 
 (provide 'setup-ido)

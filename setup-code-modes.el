8d3cba3821542a2e965fda9ffd525b860145efe8
graehl <graehl@gmail.com>
2011-12-08 16:35:30 -0800

    typedef snip

--- setup-code-modes.el
+++ setup-code-modes.el
@@ -17,7 +17,7 @@
 (set-coding-modes 'lisp scheme lisp emacs-lisp lisp-interaction)
 (set-coding-modes 'jvm java scala clojure tuareg)
 (set-coding-modes 'ml ocaml ml haskell tuareg)
-(set-coding-modes 'make make cmake makefile makefile-gmake jam fundamental conf conf-unix)
+(set-coding-modes 'make make cmake makefile makefile-gmake jam fundamental conf conf-unix change-log)
 (set-coding-modes 'doc LaTeX html)
 
 (install-coding-hooks)

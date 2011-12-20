da29fbd7317e276beb01ab42ad016341a421fb1a
graehl <graehl@gmail.com>
2011-12-20 11:58:51 -0800

    make-local-hook deprecated in emacs-24

--- setup-change-log.el
+++ setup-change-log.el
@@ -5,7 +5,8 @@
   (interactive)
   (with-whole-buffer
    (replace-string "../r" "racerx")))
+;(add-hook HOOK FUNCTION &optional APPEND LOCAL) - so make-local-hook removed in emacs 24.
 (defun setup-change-log-hook ()
-  (make-local-hook 'before-save-hook)
-  (install-hook 'before-save-hook 'cleanup-change-log))
+;  (make-local-hook 'before-save-hook)
+  (install-hook 'before-save-hook 'cleanup-change-log nil t))
 (install-hook 'change-log-mode-hook 'setup-change-log-hook)

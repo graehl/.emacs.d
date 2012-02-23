ca986c43115945ae77d98b1b420e9e741872176a
graehl <graehl@gmail.com>
2012-02-22 19:47:17 -0800

    keyboard-really-quit

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -1,6 +1,14 @@
 ;; Buffer-related defuns
 (provide 'buffer-defuns)
 
+(defun stop-using-minibuffer ()
+  (interactive)
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
+
+(add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
+
 (defun leave-minibuffer ()
   (interactive)
   (if (> (minibuffer-depth) 0) (exit-minibuffer)))
@@ -11,6 +19,7 @@
 
 (defun keyboard-really-quit ()
   (interactive)
+  (stop-using-minibuffer)
   (quit-minibuffer)
   (keyboard-quit))
 

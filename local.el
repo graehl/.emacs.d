33f26cf7d5bb36a8cc256b802edcc8bb98d392c6
Jonathan Graehl <jonathan@graehl.org>
2012-03-12 20:02:41 -0700

    windows-octave

--- local.el
+++ local.el
@@ -2,3 +2,4 @@
 (setq compile-command (with-sh "buildgraehl forest-em"))
 (setq compile-command (with-sh "HYPERGRAPH_DBG=6 LAZYK_DBG=10 test=Hypergraph/Best raccm Debug"))
 (setq compile-command (with-sh "HYPERGRAPH_DBG=6 HGBEST_DBG=10 LAZYK_DBG=10 tests=Hypergraph/Best racm Debug"))
+(setq inferior-octave-program "C:\\cache\\Octave3.4.3_gcc4.5.2\\bin\\octave.exe")

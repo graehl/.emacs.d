22ca41febeb510cdbf120123880c941664ded43c
Jonathan Graehl <jonathan@graehl.org>
2012-03-12 22:48:21 -0700

    octave system-name

--- local.el
+++ local.el
@@ -2,4 +2,6 @@
 (setq compile-command (with-sh "buildgraehl forest-em"))
 (setq compile-command (with-sh "HYPERGRAPH_DBG=6 LAZYK_DBG=10 test=Hypergraph/Best raccm Debug"))
 (setq compile-command (with-sh "HYPERGRAPH_DBG=6 HGBEST_DBG=10 LAZYK_DBG=10 tests=Hypergraph/Best racm Debug"))
-;;(setq inferior-octave-program "C:\\cache\\Octave3.4.3_gcc4.5.2\\bin\\octave.exe")
+(when (string= system-name "LATTE")
+  (setq inferior-octave-program "C:\\octave\\Octave3.6.1_gcc4.6.2\\bin\\octave.exe")
+)

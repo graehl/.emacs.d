f953819c964ef1d6d692fedc1dcf5bd17202a3bc
graehl <graehl@gmail.com>
2013-06-02 02:59:01 -0700

    disable helm m-x

--- mac.el
+++ mac.el
@@ -34,13 +34,14 @@
 ;; keybinding to toggle full screen mode
 (global-set-key (quote [M-f10]) 'toggle-frame-fullscreen)
 
+(setq on-mac-port nil)
 (defun toggle-frame-fullscreen-mac-port ()
   "Toggle full screen"
   (interactive)
   (set-frame-parameter
    nil 'fullscreen
    (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
-;;(global-set-key (quote [M-f10]) 'toggle-frame-fullscreen-mac-port)
+(when on-mac-port (global-set-key (quote [M-f10]) 'toggle-frame-fullscreen-mac-port))
 
 ;; Move to trash when deleting stuff
 (setq delete-by-moving-to-trash t
--- setup-helm.el
+++ setup-helm.el
@@ -2,7 +2,4 @@
 (require 'git-show)
 (provide 'setup-helm)
 (setq helm-input-idle-delay 0.05)
-(global-set-key (kbd "C-c h") 'helm-mini)
-(global-set-key (kbd "M-i") 'helm-semantic-or-imenu)
-(global-set-key (kbd "C-x y") 'helm-show-kill-ring)
-(global-set-key (kbd "M-x") 'helm-M-x)
+;;(global-set-key (kbd "M-x") 'helm-M-x)

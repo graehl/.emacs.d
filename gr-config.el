727a5e298d4cc1b2174555c4eface520537f7190
graehl <graehl@gmail.com>
2013-06-03 21:13:55 -0700

    gr-on-24

--- gr-config.el
+++ gr-config.el
@@ -9,9 +9,27 @@
 
 (defvar gr-have-ag (not gr-on-win))
 
+(defun emacs-version-get-component (component)
+  (let ((old-match-data (match-data))
+	(version 0)
+	(regexp (cond
+		 ((eq 'major component) "^\\([0-9]+\\)")
+		 ((eq 'minor component) "^[0-9]+\\.\\([0-9]+\\)")
+		 ((eq 'build component) "^[0-9]+\\.[0-9]+\\.\\([0-9]+\\)"))))
+    (unwind-protect
+	(and (string-match regexp emacs-version)
+	     (setq version
+		   (string-to-number (substring emacs-version
+					     (match-beginning 1)
+					     (match-end 1)))))
+      (store-match-data old-match-data))
+    version))
+
 (defun emacs-version-major ()
   "Returns (as an integer) the major version number."
   (interactive)
   (emacs-version-get-component 'major))
+
+(setq gr-on-24 (>= (emacs-version-major) 24))
 
 (provide 'gr-config)

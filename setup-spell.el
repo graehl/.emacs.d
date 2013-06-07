1fce4c90adbd063133ee93f0e2f927a12aa0730f
graehl <graehl@gmail.com>
2013-06-06 17:31:01 -0700

    spell-mode

--- setup-spell.el
+++ setup-spell.el
@@ -1,5 +1,8 @@
 (setq ispell-process-directory (expand-file-name "~/"))
-(setq ispell-program-name "hunspell")
+;(setq ispell-program-name "hunspell")
 ;(setq ispell-dictionary "english")
 (setq ispell-dictionary "en_US")
+(add-to-list 'ispell-dictionary-alist '("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil
+  ("-B")
+  nil utf-8))
 (provide 'setup-spell)
